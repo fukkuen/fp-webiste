8efe1e44c96745d52681f64f1cee8186de7639fd
fukkuen <fukkuen@gmail.com>
2018-09-25 13:59:05 +0800

    change port from 3000 to 8080

--- backend/index.js
+++ backend/index.js
@@ -49,6 +49,6 @@ app.get('/api/posts/:id', (req, res) => {
   })
 })
 
-app.listen(3000, () => {
-  console.log('Server listening on port 3000')
+app.listen(8080, () => {
+  console.log('Server listening on port 8080')
 })
